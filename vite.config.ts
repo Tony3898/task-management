bdcdc821634502e85b63b57c7d56cc42e9057b3e
tejas <tejas@afreespace.com>
2025-07-06 03:57:30 +0530

    Update Vite configuration to include server settings, build options, and path aliases; add 'build' to .gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 5001,
+    open: true,
+  },
+  build: {
+    outDir: 'build',
+    sourcemap: true,
+    assetsDir: 'assets',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+    },
+  },
+  assetsInclude: ['**/*.svg', '**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.webp'],
+}) 
